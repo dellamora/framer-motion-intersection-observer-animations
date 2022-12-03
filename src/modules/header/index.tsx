1e7f57a9d02ec4893d7de8e3ee54f75e4d9b4898
Dellamora <francielle@dellamora.dev>
2022-12-03 15:18:40 -0300

    add header desktop basis

--- src/modules/header/index.tsx
+++ src/modules/header/index.tsx
@@ -1,13 +1,29 @@
+import Link from "next/link";
 import React from "react";
 
+const links = [
+    { name: "Example Two", to: "#exampleOne", id: 1},
+    { name: "Example One", to: "#exampleTwo", id: 3 },
+    { name: "Example Three", to: "#exampleThree", id: 3 },
+  ];
 interface HeaderProps {
   children?: React.ReactNode;
 }
 const Header: React.FC<HeaderProps> = ({ children }): JSX.Element => {
+    
   return (
-    <div>
-      <h1> Hello, Header!</h1>
-      {children}
+    <div className="fixed overflow-hidden z-10">
+        <nav className="shrink-0 w-screen flex p-4  text-gray items-center justify-between bg-primary ">
+            <Link href="https://twitter.com/francidellamora"> Dellamora</Link>
+            <div className="relative flex gap-3 items-center ">
+             {links.map(({ name, to, id }) => (
+                <a className="pl-0 px-2 text-primaryDark" key={id} href={to}>
+                    {name}
+                </a>
+             ))}
+         </div>
+        <Link href="https://twitter.com/francidellamora" className=""> Buy Me A Coffee!</Link>
+        </nav>
     </div>
   );
 };
