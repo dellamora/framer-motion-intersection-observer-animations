92b3aa0d91b6525dcd2e779c819fd5c4e39fda37
Dellamora <francielle@dellamora.dev>
2022-12-03 15:58:51 -0300

    add section component

---
+++ src/common/components/section.tsx
@@ -0,0 +1,38 @@
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
+type Props = {
+  id: string;
+  children: JSX.Element | JSX.Element[];
+  setIsInView?: (isInView: boolean) => void;
+};
+
+export const Section = ({
+  id,
+  children,
+  setIsInView,
+
+}: Props): JSX.Element => {
+  const { ref, inView } = useInView({
+    threshold: 0.2,
+    triggerOnce: true,
+  });
+
+  useEffect(() => {
+    if (setIsInView) {
+      setIsInView(inView);
+    }
+  }, [inView]);
+  
+  return (
+    <section
+      className="h-screen relative overflow-hidden p-[65px]"
+      ref={ref}
+      
+    >
+      <div id={id} className="absolute pointer-events-none -top-[20px]"/>
+      {children}
+    </section>
+  );
+};
+
+export default Section;
