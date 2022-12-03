fc8bc6cd9512e0ed4cf63f82bef941b7a614e9a5
Dellamora <francielle@dellamora.dev>
2022-12-03 15:18:25 -0300

    add color palettte in tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      primary: "#FFFFFF",
+      primaryLight: "#FBFBFB",
+      primaryDark: "#1E1E1E",
+      redLight: "#F83941",
+      redDark: "#D91A23",
+      gray: "#232E35",
+      grayMedium: "#656D72",
+      grayLight: "#D9D9D9", //span e textos
+      whiteBasic: " #F5F3FE",
+      whiteTextDarkMode: "#F1F1F1",
+      secondaryDark: "#131313",
+    },
     extend: {},
   },
   plugins: [],
