98eae62e910fe8e455e158111ef2271cd1c75f20
Dellamora <francielle@dellamora.dev>
2022-12-03 15:59:00 -0300

    clean based layout

--- src/common/layouts/basedLayout.tsx
+++ src/common/layouts/basedLayout.tsx
@@ -1,4 +1,3 @@
-// import { useScroll } from "framer-motion";
 import React, { useRef } from "react";
 import Footer from "../../modules/footer";
 import Header from "../../modules/header";
@@ -7,10 +6,7 @@ const BaseLayout = ({
 }: {
   children: React.ReactNode;
 }): JSX.Element => {
-//   const containerRef = useRef(null)
-//   const { scrollYProgress } = useScroll({
-//   container: containerRef
-//   });
+
   return (
     <div className="h-screen w-screen overflow-y-hidden">
     <div id="modal-root"/>
