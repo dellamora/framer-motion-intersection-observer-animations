0a61b060d5592a27ab649aba04d6d9ed0fda12a2
Dellamora <francielle@dellamora.dev>
2022-12-03 14:42:36 -0300

    create t3 template

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
+  swcMinify: true,
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+};
+export default config;
