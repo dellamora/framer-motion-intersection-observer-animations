756273ae4ea96335cf66f97a014875b38b95d0d6
Dellamora <francielle@dellamora.dev>
2022-12-03 22:12:17 -0300

    WIP: idk what im doing

--- src/common/components/animation.tsx
+++ src/common/components/animation.tsx
@@ -8,15 +8,14 @@ interface AnimationProps {
 const Animation: React.FC<AnimationProps> = ({ children,inView }): JSX.Element => {
   return (
     <motion.div 
-        className="flex flex-col gap-3"
+        className="flex flex-col"
         initial="hidden"
         animate={inView ? "view" : "hidden"}  
     >
     <motion.div
       variants={{
         hidden: {opacity: 0}, 
-        view: {opacity: 1,
-          transition:{delay: 2}}
+        view: {opacity: 1,}
       }}  
     >
         {children}
