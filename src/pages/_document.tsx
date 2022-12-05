cea0e37efc01b8731934d82b46ce664f2b466cc3
Dellamora <francielle@dellamora.dev>
2022-12-05 17:50:31 -0300

    add SEO tags

---
+++ src/pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta name="description" content="some cool animations with Framer Motion and Intersection Observer API in React" />
+        <meta name="author" content="Francielle Dellamora"  />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,18 @@
 import { type NextPage } from "next";
+import Head from "next/head";
 import ExampleOne from "../modules/exampleOne/exampleOne";
+import ExampleThree from "../modules/exampleThree/exampleThree";
 import ExampleTwo from "../modules/exampleTwo/exampleTwo";
 
 const Home: NextPage = () => {
   return (
     <>
+    <Head>
+      <title>Dellamora animations</title>
+    </Head>
     <ExampleOne/>
     <ExampleTwo/>
+    <ExampleThree/>
     </>
   );
 };
