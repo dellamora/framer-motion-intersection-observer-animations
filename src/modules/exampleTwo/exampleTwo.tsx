56ae77f1828d2154a8fdac0aabd13340c80d25db
Dellamora <francielle@dellamora.dev>
2022-12-05 17:52:06 -0300

    add title and animate the images

--- src/modules/exampleTwo/exampleTwo.tsx
+++ src/modules/exampleTwo/exampleTwo.tsx
@@ -2,6 +2,9 @@ import React, { useState , useEffect } from "react";
 import Section from "../../common/components/section";
 import Animation from "../../common/components/animation";
 import { motion } from "framer-motion";
+import { InView } from "react-intersection-observer";
+import Title from "../../common/components/titleAnimation";
+
 
 interface ExampleTwoProps {
   children?: React.ReactNode;
@@ -15,25 +18,35 @@ const ExampleTwo: React.FC<ExampleTwoProps> = ({ children }): JSX.Element => {
       setIsInView={state => {
         setInView(state);
       }}
-    >
-      <Animation inView={inView}>
+      >
+      <Title title="Now you can play with your scroll" triggerAnimation={inView}/>
+      <Animation 
+      inView={inView}
+      >
       <motion.img
-
+       initial={{y: 0, x: -200, rotate: "-24deg"}}
+       animate={{y:0, x:-10, rotate: 0, transition: {type: "spring", stiffness: 50}}}  
         src="/leftGirl.png"
-        className=" absolute top-0 left-0 w-[21rem] h-[25rem]"
+        className=" absolute top-0 left-0  h-[25rem] lg:h-[35rem]"
       />
      <motion.img
-      src="/leftDownGirl.png"
-      className=" absolute bottom-0 left-0 w-[21rem] h-[25rem]"
+       initial={{y: 0, x: -200, rotate: "-12deg"}}
+       animate={{y:0, x:-10, rotate: 0, transition: {type: "spring", stiffness: 50}}}  
+       src="/leftDownGirl.png"
+       className=" absolute bottom-0 left-0  h-[21rem] lg:h-[25rem]"
       />
-      <h1 className="text-3xl">somethuing idiot here</h1>
+      
       <motion.img
-      src="/rightGirl.png"
-      className=" absolute top-0 -right-28 w-[21rem] h-[25rem]"
+        initial={{y: 0, x: 200, }}
+        animate={{y:0, x:15, transition: {type: "spring", stiffness: 50}}}  
+        src="/rightGirl.png"
+        className=" absolute top-0 -right-20  h-[25rem] lg:h-[30rem]"
     />
       <motion.img
-      src="/l.png"
-      className=" absolute bottom-0 -right-0 w-80 "
+        initial={{y: 0, x:200, rotate: "6deg" }}
+        animate={{y:0, x:10, rotate: 0, transition: {type: "spring", stiffness: 50}}}       
+        src="/l.png"
+        className=" absolute bottom-0 -right-0  h-[22rem] lg:h-[24rem] "
     />
       </Animation>
 
