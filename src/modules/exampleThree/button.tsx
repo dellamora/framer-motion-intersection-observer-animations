e7c396c3c80441d4124876b99173d85710631a5d
Dellamora <francielle@dellamora.dev>
2022-12-06 12:17:37 -0300

    add className props

--- src/modules/exampleThree/button.tsx
+++ src/modules/exampleThree/button.tsx
@@ -2,7 +2,6 @@
 
 import { motion } from "framer-motion";
 import React from "react";
-import ChevronRight from "../../common/svgs/chevronRight";
 
 type Props = {
   onClick: () => void;
@@ -22,7 +21,7 @@ const CompanyButton = ({
       animate={inView ?  {opacity: 1,transition:{delay: 1}} : {opacity: 0}} 
     >
       <button
-        className={`w-1/2 flex relative font-Inter items-center text-xl text-left py-2 md:px-6 px-4 z-20 
+        className={` flex relative font-Inter items-center text-xl text-left py-2 md:px-6 px-4 z-20 
         ${active ? "text-redLight" : "text-grayMedium"}  `}
         onClick={onClick}
       >
@@ -31,7 +30,7 @@ const CompanyButton = ({
       {active && (
         <motion.div
           className=" rounded absolute top-0 bottom-0 left-0 right-0 bg-whiteBasic  z-10 flex justify-end"
-          layoutId="companyBg"
+          layoutId="buttonBg"
         />
       )}
     </motion.div>
