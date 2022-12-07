e802a4f0cc7b2b37a0b173b0a3446f576fc0c15b
Dellamora <francielle@dellamora.dev>
2022-12-07 11:18:30 -0300

    removed animation component and aniamte

--- src/modules/exampleThree/index.tsx
+++ src/modules/exampleThree/index.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Section } from "../../common/components/section";
 import CompanyButton from "./button";
 import Animation from "../../common/components/animation";
+import { motion, MotionConfig } from "framer-motion";
 
 
 const buttons = [
@@ -9,11 +10,17 @@ const buttons = [
   {name: "option 2", id: 2}
 ]
 
-
 const ExampleThree = (): JSX.Element => {
   const [inView, setInView] = useState(false);
   const [activeBtn, setActiveBtn] = useState(buttons[0].id);
-
+  const animations = {
+    hidden: {
+      opacity: 0
+    },
+    view: {
+      opacity: 1,
+    }
+   }
   return (
     <Section 
       className="min-h-[70vh]"
@@ -22,27 +29,31 @@ const ExampleThree = (): JSX.Element => {
         setInView(curr => state || curr);
       }}
     >
-     <Animation inView={inView}>
-      <div className="flex gap-8 md:gap-16 flex-col md:flex-row justify-center ">
+      <motion.div
+        initial="hidden"
+        animate={inView ? "view" : "hidden"}
+        variants={animations}
+        transition={{delay: 0.5}}
+      >
+       <div className="flex gap-8 md:gap-16 flex-col md:flex-row justify-center ">
         <div className="w-full md:w-1/2 flex md:flex-col justify-between">
         {buttons.map(({name, id}) => {
          return  (
           <CompanyButton
-          key={`btn-${id}`}
-          active={activeBtn === id}
-          inView={inView}
-          onClick={() => { setActiveBtn(id)}}
+            key={`btn-${id}`}
+            active={activeBtn === id}
+            onClick={() => { setActiveBtn(id)}}
           >
             {name}
           </CompanyButton>
          )
         })}
-      </div>
+       </div>
        </div>
        <h1 className="mt-20 w-full flex justify-center text-center text-sm text-grayMedium">
         Now you can create your own animations!<br/>or just stress yourself {`(:`}
        </h1>
-    </Animation>
+      </motion.div>
     </Section>
   );
 };
